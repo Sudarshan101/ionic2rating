ff6b67007de9af9ee81c513f58ce8e6244b757c4
Sudarshan101 <sudarshan.vis101@gmail.com.com>
2018-05-03 16:28:09 +0530

    rating code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Ionic2RatingModule } from "ionic2-rating";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  rate : any = 0;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onModelChange(event){
+  	this.rate = event;
+  	console.log(event);
   }
 
 }
